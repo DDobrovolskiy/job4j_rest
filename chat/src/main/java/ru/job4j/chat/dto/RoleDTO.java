4b77840deac45246a4bb6f4a499b12df001fe35f
DDobrovolskiy <dda.90@mail.ru>
2021-09-27 22:09:04 +0500

    7. DTO [#504800]

--- chat/src/main/java/ru/job4j/chat/dto/RoleDTO.java
+++ chat/src/main/java/ru/job4j/chat/dto/RoleDTO.java
@@ -3,13 +3,13 @@ package ru.job4j.chat.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 @Getter
 @Setter
 public class RoleDTO {
     private long id;
     private String title;
-    private Set<Long> personsID = new HashSet<>();
+    private List<Long> personsID = new LinkedList<>();
 }
--- chat/src/main/java/ru/job4j/chat/dto/RoomDTO.java
+++ chat/src/main/java/ru/job4j/chat/dto/RoomDTO.java
@@ -3,14 +3,14 @@ package ru.job4j.chat.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
+import java.util.List;
 
 @Getter
 @Setter
 public class RoomDTO {
     private long id;
     private String name;
-    private Set<Long> messagesID = new HashSet<>();
-    private Set<Long> personsID = new HashSet<>();
+    private List<Long> messagesID = new LinkedList<>();
+    private List<Long> personsID = new LinkedList<>();
 }
